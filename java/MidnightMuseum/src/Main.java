c6b643b288425cbce605cea64e06aa21ead598f6
hossdia <hossainmahdia22@gmail.com>
2026-08-01 17:23:28 +0600

    feat: take player name and greet

--- java/MidnightMuseum/src/Main.java
+++ java/MidnightMuseum/src/Main.java
@@ -1,11 +1,43 @@
+import java.util.Scanner;
+
+
+/*
+ * Midnight Museum
+ *
+ * Sprint 1
+ * MM-001 - Night Curator Introduction
+ *
+ * TODO
+ * [ ] Print title
+ * [ ] Ask player's name
+ * [ ] Store player's name
+ * [ ] Display personalized welcome
+ */
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("====================================");
-        System.out.println("      MIDNIGHT MUSEUM");
-        System.out.println("====================================");
-        System.out.println("The museum opens at midnight...");
-        System.out.println("Welcome, Night Curator.");
-
+        Scanner scanner = new Scanner(System.in);
+        String curatorName;
+        System.out.println("========================================");
+        System.out.println("         MIDNIGHT MUSEUM");
+        System.out.println("========================================");
+        System.out.println();
+        System.out.println("The entrance doors close behind you.");
+        System.out.println();
+        System.out.println("A voice echoes through the empty halls...");
+        System.out.println();
+        System.out.print("\"State your name, Curator.\"\n\n> ");
+        curatorName = scanner.nextLine();
+        System.out.println();
+        System.out.println("Welcome, " + curatorName + ".");
+        System.out.println();
+        System.out.println("You have been appointed as the Night Curator.");
+        System.out.println();
+        System.out.println("The museum opens only after sunset.");
+        System.out.println();
+        System.out.println("Protect the artifacts.");
+        System.out.println("Contain the anomalies.");
+        System.out.println();
+        System.out.println("May you flourish in your mission to protect this museum.");
+        scanner.close();
     }
 }
