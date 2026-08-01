f453bd8e002c8d916b21d91cfd19f59e5aae8c11
hossdia <hossainmahdia22@gmail.com>
2026-08-01 17:05:36 +0600

    feat: initialize Midnight Museum project

---
+++ java/MidnightMuseum/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("      MIDNIGHT MUSEUM");
+        System.out.println("====================================");
+        System.out.println("The museum opens at midnight...");
+        System.out.println("Welcome, Night Curator.");
+
+    }
+}
