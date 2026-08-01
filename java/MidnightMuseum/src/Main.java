a1e9352b2d912a09c950eaf40a7b6bfcdbef85b8
hossdia <hossainmahdia22@gmail.com>
2026-08-01 17:26:19 +0600

    feat: printTitle method created

--- java/MidnightMuseum/src/Main.java
+++ java/MidnightMuseum/src/Main.java
@@ -1,32 +1,41 @@
 import java.util.Scanner;
 
-
 /*
  * Midnight Museum
  *
- * Sprint 1
- * MM-001 - Night Curator Introduction
+ * Version History
+ * ----------------------------
+ * Sprint 0 - Project initialized
+ * Sprint 1 - MM-001 Curator introduction
  *
- * TODO
- * [ ] Print title
- * [ ] Ask player's name
- * [ ] Store player's name
- * [ ] Display personalized welcome
+ * Sprint 1 Checklists
+ * [x] Print title
+ * [x] Ask player's name
+ * [x] Store player's name
+ * [x] Display personalized welcome
  */
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String curatorName;
+
+    public static void printTitle() {
         System.out.println("========================================");
         System.out.println("         MIDNIGHT MUSEUM");
         System.out.println("========================================");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String curatorName;
+
+        printTitle();
         System.out.println();
         System.out.println("The entrance doors close behind you.");
         System.out.println();
         System.out.println("A voice echoes through the empty halls...");
         System.out.println();
         System.out.print("\"State your name, Curator.\"\n\n> ");
+
         curatorName = scanner.nextLine();
+
         System.out.println();
         System.out.println("Welcome, " + curatorName + ".");
         System.out.println();
@@ -38,6 +47,7 @@ public class Main {
         System.out.println("Contain the anomalies.");
         System.out.println();
         System.out.println("May you flourish in your mission to protect this museum.");
+
         scanner.close();
     }
 }
